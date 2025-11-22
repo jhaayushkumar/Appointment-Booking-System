c7933e37621ca5cba1c2f7372ab0b2981017ebc2
adarsh-priydarshi-5646 <adarshpriydarshi5646@gmail.com>
2025-11-22 15:37:52 +0530

    Connect frontend with backend API

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,6 +3,11 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import {BrowserRouter} from 'react-router-dom'
+import axios from 'axios'
+
+axios.defaults.withCredentials = true
+axios.defaults.baseURL = 'http://localhost:3000'
+
 createRoot(document.getElementById('root')).render(
   <BrowserRouter>
     <App />
